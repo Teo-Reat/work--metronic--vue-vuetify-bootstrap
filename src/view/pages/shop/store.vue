bec80a33ddad08fe84536c996e5d821958de67b3
Teo-Reat <teo.reat@gmail.com>
2021-02-09 17:32:11 +0300

    Error resolved

--- src/view/pages/shop/store.vue
+++ src/view/pages/shop/store.vue
@@ -15,54 +15,56 @@
           Test
         </router-link>
         <b-card
-          v-for="shop in storeList"
-          :key="shop._id"
-          :title="shop.name.en"
-          :sub-title="shop.name.heb"
-          no-body
-          class="overflow-hidden my-8"
+            v-for="shop in storeList"
+            :key="shop._id"
+            :title="shop.name ? shop.name.en : ' - '"
+            :sub-title="shop.name ? shop.name.heb : ' - '"
+            no-body
+            class="overflow-hidden my-8"
         >
           <b-row no-gutters>
             <b-col md="4">
-              <b-card-body :title="shop.name.en + ' | ' + shop.name.heb">
-                <b-card-text>
-                  {{ shop.address.en }} | {{ shop.address.heb }}
-                </b-card-text>
-                <router-link
-                  :to="{
+              <template v-if="shop.name">
+                <b-card-body :title="shop.name.en + ' | ' + shop.name.heb">
+                  <b-card-text>
+                    {{ shop.address.en }} | {{ shop.address.heb }}
+                  </b-card-text>
+                  <router-link
+                      :to="{
                     name: 'shop_detail',
                     params: { id: shop._id, shop: shop }
                   }"
-                  class="card-link"
-                >
-                  Detail
-                </router-link>
-              </b-card-body>
+                      class="card-link"
+                  >
+                    Detail
+                  </router-link>
+                </b-card-body>
+              </template>
             </b-col>
             <b-col md="8" style="display: flex; justify-content: flex-end">
               <b-card-img
-                v-if="shop.pic[0]"
-                :src="
+                  v-if="shop.pic[0]"
+                  :src="
                   'https://backend.hashve.co.il/assets/store/' + shop.pic[0]
                 "
-                class="rounded-0"
-                style="max-height: 180px; width: 30%; object-fit: cover"
+                  class="rounded-0"
+                  style="max-height: 180px; width: 30%; object-fit: cover"
               ></b-card-img>
               <b-card-img
-                v-if="shop.pic[1]"
-                :src="
+                  v-if="shop.pic[1]"
+                  :src="
                   'https://backend.hashve.co.il/assets/store/' + shop.pic[1]
                 "
-                class="rounded-0"
-                style="max-height: 180px; width: 30%; object-fit: cover"
+                  class="rounded-0"
+                  style="max-height: 180px; width: 30%; object-fit: cover"
               ></b-card-img>
               <b-card-img
-                v-if="shop.pic[2]"
-                :src="
+                  v-if="shop.pic[2]"
+                  :src="
                   'https://backend.hashve.co.il/assets/store/' + shop.pic[2]
                 "
-                class="rounded-0"
-                style="max-height: 180px; width: 30%; object-fit: cover"
+                  class="rounded-0"
+                  style="max-height: 180px; width: 30%; object-fit: cover"
               ></b-card-img>
             </b-col>
           </b-row>
@@ -73,26 +75,26 @@
 </template>
 
 <script>
-import { GET_STORE_LIST } from "@/core/services/store/store.module";
-import { SET_BREADCRUMB } from "@/core/services/store/breadcrumbs.module";
+import {GET_STORE_LIST} from "@/core/services/store/store.module";
+import {SET_BREADCRUMB} from "@/core/services/store/breadcrumbs.module";
 
 export default {
   name: "shop",
 
   data: () => {
     return {
-      storeList: []
+      storeList: null
     };
   },
 
   mounted() {
     this.$store.dispatch(SET_BREADCRUMB, [
-      { title: "Dashboard", route: "" },
-      { title: "Shops" }
+      {title: "Dashboard", route: ""},
+      {title: "Shops"}
     ]);
     this.$store
-      .dispatch(GET_STORE_LIST)
-      .then(data => (this.storeList = data));
+        .dispatch(GET_STORE_LIST)
+        .then(data => (this.storeList = data));
   }
 };
 </script>
